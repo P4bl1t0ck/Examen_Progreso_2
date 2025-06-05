abe33efd07f4c2f810cb4426713d9a48ca5dcf3b
Pablo.Montalvo <pablo.montalvo.marroquin@udla.edu.ec>
2025-06-05 08:57:13 -0500

    Creadas el modelo y el repositorio de chiste, tambien se creara la modoficacion para que imprima en la pantalla.Logre crear el Json en chiste y instale l paquete de nuget faltanten.

---
+++ ExamenProgreso2/Models/Chiste.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenProgreso2.Models
+{
+    public class Chiste {
+
+        //Parecido al JSON que se recibe de la API
+        /*{
+         "type": "general",
+         "setup": "Why do crabs never give to charity?",
+         "punchline": "Because they’re shellfish.",
+         "id": 346
+         }*/
+        public string type { get; set; }
+        public string setup { get; set; }
+        public string punchline { get; set; }
+        public int id { get; set; }
+    }
+}
---
+++ ExamenProgreso2/Repository/ChistesRepository.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using ExamenProgreso2.Models;
+namespace ExamenProgreso2.Repository
+{
+    public class ChistesRepository
+    {
+        private HttpClient _httpClient;
+        public ChistesRepository() {
+            _httpClient = new HttpClient();
+        }
+        public async Task<IEnumerable<Chiste>> GetCountryListAsync()
+        {
+            string url = "https://official-joke-api.appspot.com/random_joke";
+            string response_json = await _httpClient.GetStringAsync(url);
+            Chiste chiste = JsonConvert.DeserializeObject<Chiste>(response_json);
+            return new List<Chiste> { chiste };
+        }
+    }
+}
