9201faa022ee4d3560d54385ef0310c09651576c
Pablo.Montalvo <pablo.montalvo.marroquin@udla.edu.ec>
2025-06-05 09:01:23 -0500

    Creacion del copdigo para la solicitud de Api.

--- ExamenProgreso2/chistespueste.xaml.cs
+++ ExamenProgreso2/chistespueste.xaml.cs
@@ -1,9 +1,24 @@
 namespace ExamenProgreso2;
-
+using ExamenProgreso2.Repository;
+using ExamenProgreso2.Models;
 public partial class chistespueste : ContentPage
 {
-	public chistespueste()
+    private ChistesRepository _chistesRepository = new ChistesRepository();
+    public chistespueste()
 	{
 		InitializeComponent();
 	}
+    private async void OnObtenerChisteClicked(object sender, EventArgs e)
+    {
+        var chistes = await _chistesRepository.GetCountryListAsync();
+        var chiste = chistes.FirstOrDefault();
+        if (chiste != null)
+        {
+            ChisteLabel.Text = $"{chiste.setup}\n\n{chiste.punchline}";
+        }
+        else
+        {
+            ChisteLabel.Text = "No se pudo obtener un chiste.";
+        }
+    }
 }
